efe3eca755c3a3add624f22a5f3f4028e42af8b7
oneofhersisters <blankinsearch@gmail.com>
2026-05-11 09:51:26 +0200

    Add HTML Tag Decorator solution

---
+++ HTML-Tag-Decorator.py
@@ -0,0 +1,39 @@
+from typing import Callable
+
+
+def add_tag(tag: str):
+    def wrapper(func: Callable):
+        def inner(*args, **kwargs):
+            res = func(*args, **kwargs)
+            return f"<{tag}>{res}</{tag}>"
+        return inner
+    return wrapper
+
+
+def add_div(func: Callable):
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        return f"<div>{res}</div>"
+    return wrapper
+
+
+@add_tag("h2")
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
+
+print(greeting("Anton"))   # <h2>Hello, Anton!</h2>
+
+
+@add_div
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
+
+print(greeting("Grisha"))  # <div>Hello, Grisha!</div>
+
+
+@add_tag("h2")
+@add_div
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
+
+print(greeting("Maksim"))  # <h2><div>Hello, Maksim!</div></h2>
